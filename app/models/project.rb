cd22251b59943d6fee158cac36ea9c0ba43bb03d
Nate Duffy <nate.duffy@gmail.com>
2015-12-31 14:35:25 -0500

    added task partial

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
 class Project < ActiveRecord::Base
   has_many :tasks
+
+  after_initialize :default_values
+
+  def to_s
+    self.description
+  end
+
+  private
+
+  def default_values
+    self.complete ||= false
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,29 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+  acts_as_taggable
+
+  after_initialize :default_values
+
+  def self.active
+    Task.where(complete: false)
+  end
+
+  def to_s
+    display = self.description
+=begin
+    display = self.description
+    if self.project.present?
+      display = display + "\n\t^ Project: " + self.project.to_s
+    end
+    display
+=end
+  end
+
+  private
+
+  def default_values
+    self.complete ||= false
+    self.processed ||= false
+  end
+
 end
